eb68e983af19555ea951026b5206cab6162ecaa8
Govind Kumar Yadav <robertgovind@gmail.com>
2023-10-26 21:26:19 +0545

    modified

--- task01_todo_app/lib/home.dart
+++ task01_todo_app/lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task01_todo_app/pages/landing_page.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -10,6 +11,137 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            "ToDo App",
+          ),
+        ),
+        backgroundColor: const Color.fromARGB(255, 164, 75, 180),
+      ),
+      body: Container(
+        color: const Color.fromARGB(255, 164, 75, 180),
+        child: ListView(
+          children: [
+            Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(20),
+                  height: 300,
+                  decoration: BoxDecoration(
+                    color: Colors.amber,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      border: Border.all(color: Colors.black)),
+                  margin: const EdgeInsets.only(
+                      left: 12, right: 12, top: 12, bottom: 12),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                        prefixIcon: Icon(
+                          Icons.email,
+                          size: 30,
+                        ),
+                        fillColor: Colors.white,
+                        hintText: "Enter Email",
+                        contentPadding: EdgeInsets.all(12),
+                        border: InputBorder.none),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      border: Border.all(color: Colors.black)),
+                  margin:
+                      const EdgeInsets.only(left: 12, right: 12, bottom: 12),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                        prefixIcon: Icon(
+                          Icons.lock,
+                          size: 30,
+                        ),
+                        suffixIcon: Icon(Icons.remove_red_eye),
+                        fillColor: Colors.white,
+                        hintText: "Enter Password",
+                        contentPadding: EdgeInsets.all(12),
+                        border: InputBorder.none),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      border: Border.all(color: Colors.black)),
+                  margin:
+                      const EdgeInsets.only(left: 12, right: 12, bottom: 12),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                        prefixIcon: Icon(
+                          Icons.person,
+                          size: 30,
+                        ),
+                        fillColor: Colors.white,
+                        hintText: "Enter Username",
+                        contentPadding: EdgeInsets.all(12),
+                        border: InputBorder.none),
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  margin: const EdgeInsets.symmetric(horizontal: 12),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateColor.resolveWith(
+                        (states) => const Color.fromARGB(255, 150, 63, 92),
+                      ),
+                    ),
+                    child: const Text(
+                      "Signin",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Already have an account",
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+                const Text("--------------OR-----------------"),
+                Container(
+                  width: double.infinity,
+                  margin: const EdgeInsets.symmetric(horizontal: 12),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateColor.resolveWith(
+                        (states) => const Color.fromARGB(255, 150, 63, 92),
+                      ),
+                    ),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                          "Signin with google",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        Icon(Icons.circle),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- task01_todo_app/lib/main.dart
+++ task01_todo_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:task01_todo_app/home.dart';
+import 'package:task01_todo_app/pages/landing_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,8 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "ToDo app",
-      home: Home(),
+      home: LandingPage(),
     );
   }
 }
