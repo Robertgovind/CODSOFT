5a24f0de1bb246930e206ffe4a1f87f9d0d4f02b
Govind Kumar Yadav <robertgovind@gmail.com>
2023-11-13 09:02:02 +0545

    final stage

--- task04_quize/lib/screens/result_screen.dart
+++ task04_quize/lib/screens/result_screen.dart
@@ -1,28 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:task04_quize/constants.dart';
+import 'package:task04_quize/screens/quiz_screen.dart';
 
 class ResultScreen extends StatelessWidget {
-  const ResultScreen({super.key, this.score});
+  const ResultScreen({super.key, this.score = 0});
   final score;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        margin:const EdgeInsets.only(left: 25,right: 25,top: 40) ,
-        decoration: BoxDecoration(border: Border.all(width:1 ),),
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/back.webp'), fit: BoxFit.cover),
+        ),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Text(
-              '$score/50',
-              style:const TextStyle(fontSize: 40),
+            const Text(
+              "Result",
+              style: TextStyle(fontSize: 40, color: Colors.white),
             ),
-             const Text(
-              'Congratulation',
-              style: TextStyle(fontSize: 40,color: Colors.green),
+            Container(
+              margin: const EdgeInsets.only(left: 25, right: 25, top: 40),
+              decoration: BoxDecoration(
+                border: Border.all(width: 1),
+                borderRadius: BorderRadius.circular(12),
+                boxShadow: const [
+                  BoxShadow(color: Colors.black, blurRadius: 1),
+                ],
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Congratulation!!',
+                    style: TextStyle(fontSize: 40, color: Colors.green),
+                  ),
+                  Text(
+                    '$score/50',
+                    style: const TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                  const Image(
+                    image: AssetImage('assets/trophy-star.png'),
+                  ),
+                ],
+              ),
             ),
-
-            Container(child: Image(image: AssetImage('assets/trophy-star.png'),),),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateColor.resolveWith((states) => kButtonColor),
+                ),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (BuildContext context) {
+                    return const QuizScreen();
+                  }));
+                },
+                child: const Text(
+                  'Next Question',
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+              ),
+            )
           ],
         ),
       ),
